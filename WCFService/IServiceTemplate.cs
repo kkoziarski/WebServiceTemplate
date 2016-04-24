65dffe1e71d6ab85f70bc608b5118068c78431e4
Krzysztof Koziarski <krzyhook@gmail.com>
2016-04-24 18:55:31 +0200

    Service1 rename.

--- WCFService/IServiceTemplate.cs
+++ WCFService/IServiceTemplate.cs
@@ -3,9 +3,9 @@
     using System.Runtime.Serialization;
     using System.ServiceModel;
 
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceTemplate" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IServiceTemplate
     {
 
         [OperationContract]
